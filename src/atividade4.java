f81856a0fa4982b721bf5c89c6e04b14435de48f
minlaura <laurarars@gmail.com>
2024-05-09 20:09:25 -0300

    Learning 2 & 3

---
+++ src/atividade4.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class atividade4 {
+    public static void main(String[] args) {
+
+        float altura;
+        String sexo;
+
+
+
+
+        Scanner a = new Scanner(System.in);
+        altura = a.nextFloat();
+        Scanner b = new Scanner(System.in);
+        sexo = b.next();
+
+
+//Para strings se usa equals, exemplo da atividade, comparação.
+
+
+    if (sexo.equals("Masculino")){
+        double pesoideal;
+      pesoideal =  (72.7 * altura) - 58;
+        System.out.printf("Peso ideal: %.2f\n", pesoideal);
+    } else if (sexo.equals("Feminino")){
+        double pesoideal;
+        pesoideal = (62*altura) - 44.7;
+         System.out.printf("Peso ideal: %.2f\n", pesoideal);
+        }
+
+
+
+
+    }
+}
